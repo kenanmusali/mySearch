bcf0ce9b10e5f45e851332a9bbf8cbfcefdd73d5
kenanmusali <textme0306@gmail.com>
2024-02-08 10:53:29 -0800

    1.0.2

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,18 +1,41 @@
 
+// const postsElem = document.querySelector(".posts");
+
+// let url = "https://pixabay.com/api/?key=24090419-925e057925ba4cc124682bb5f&q=${}";
+
+// fetch(url)
+//     .then(res => res.json())
+//     .then(data => {
+//         data.hits.forEach(post => {
+//             postsElem.innerHTML += `
+//                 <div class="post">
+//                     <img class="pics" src="${post.largeImageURL}">
+//                 </div>`;
+//         });
+//     });
 
 
 const postsElem = document.querySelector(".posts");
+const searchInput = document.querySelector(".search");
 
-let url = "https://pixabay.com/api/?key=24090419-925e057925ba4cc124682bb5f&q=${}";
+function fetchImages(searchQuery) {
+    let url = `https://pixabay.com/api/?key=24090419-925e057925ba4cc124682bb5f&q=${searchQuery}`;
 
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        data.hits.forEach(post => {
-            postsElem.innerHTML += `
-                <div class="post">
-                    <img class="pics" src="${post.largeImageURL}">
-                </div>`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            postsElem.innerHTML = "";
+
+            data.hits.forEach(post => {
+                postsElem.innerHTML += `
+                    <div class="post">
+                        <img class="pics" src="${post.webformatURL}">
+                    </div>`;
+            });
         });
-    });
+}
 
+fetchImages("");
+searchInput.addEventListener("input", function() {
+    fetchImages(this.value.trim());
+});
